1794a7871bc782e3af5b4d3929117394b12ad3ad
soh1826 <soh182662@gamil.com>
2019-09-06 09:07:22 +0900

    어쩌다 윈도우의 lable

--- 09_06_GUI/src/GUI1_JFrame.java
+++ 09_06_GUI/src/GUI1_JFrame.java
@@ -3,6 +3,9 @@ import javax.swing.*;
 public class GUI1_JFrame {
     public static void main(String[] args) {
         JFrame frame=new JFrame("창이름");
+
+        JLabel label = new JLabel("금요일이네.");
+        frame.add(label);
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
